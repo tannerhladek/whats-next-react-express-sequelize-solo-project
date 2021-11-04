5354103e6d2a32f719c475b4c00599c64765a304
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-11-04 13:42:18 -0700

    user auth middleware routes created for testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,48 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const { requireAuth } = require('../../utils/auth.js');
+
+
 const router = express.Router();
 
+// POST test route
 router.post('/test', function (req, res) {
    res.json({ requestBody: req.body });
 });
+
+
+// set token cookie test route
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+   const user = await User.findOne({
+      where: {
+         username: 'Demo-lition'
+      },
+   })
+   setTokenCookie(res, user);
+   return res.json({ user });
+}));
+
+// testing restore user middleware
+// GET /api/restore-user
+router.get(
+   '/restore-user',
+   restoreUser,
+   (req, res) => {
+      return res.json(req.user);
+   }
+);
+
+// testing require auth middleware
+// GET /api/require-auth
+router.get(
+   '/require-auth',
+   requireAuth,
+   (req, res) => {
+      return res.json(req.user);
+   }
+);
 
 module.exports = router;
