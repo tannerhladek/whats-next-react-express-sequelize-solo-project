5a6d02933ad1eb6fb7fa7c66a4fa8b485e8bef3e
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-11-08 12:49:14 -0800

    db created, migration files updated, model files updated and associatins defined

---
+++ backend/db/migrations/20211108191948-create-activity.js
@@ -0,0 +1,61 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Activities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING(2),
+        allowNull: false
+      },
+      country: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      lat: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      lng: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'Users' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Activities');
+  }
+};
---
+++ backend/db/models/activity.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Activity = sequelize.define('Activity', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    lng: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  }, {});
+  Activity.associate = function(models) {
+    Activity.hasMany(models.Activity_image, { foreignKey: 'activity_id', onDelete: 'CASCADE', hooks: true });
+    Activity.hasMany(models.Booking, { foreignKey: 'activity_id', onDelete: 'CASCADE', hooks: true });
+    Activity.belongsTo(models.User, { foreignKey: 'user_id' });
+  };
+  return Activity;
+};
