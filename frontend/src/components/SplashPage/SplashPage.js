ce2fbb6d955f3097075ca51a1125af45eb5fc519
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-11-13 14:59:51 -0800

    splash page image and tagline created

--- frontend/src/components/SplashPage/SplashPage.js
+++ frontend/src/components/SplashPage/SplashPage.js
@@ -6,6 +6,9 @@ const SplashPage = () => {
 
    return (
       <div id={styles.splashPageContainer}>
+         <div className={styles.splashPageImg}>
+            <span id={styles.splashPageTagline}>Not sure what to do? Perfect.</span>
+         </div>
          <ActivitiesDiv />
       </div>
    );
