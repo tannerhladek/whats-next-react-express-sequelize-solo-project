4db730eee1365a96f5cf9553ee019bed826d1010
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2021-11-14 12:13:18 -0800

    if an activity has no reviews, that is stated, and users are directed to submit a review

--- frontend/src/components/ReviewsDiv/ReviewsDiv.js
+++ frontend/src/components/ReviewsDiv/ReviewsDiv.js
@@ -8,14 +8,25 @@ const ReviewsDiv = ({ activityId }) => {
    const dispatch = useDispatch();
    const reviews = useSelector(state => state.activities[activityId].Reviews);
 
-   return (
-      <div className={styles.reviewContainer}>
-         <h1>Reviews</h1>
+   let reviewsContent;
+   if (reviews.length === 0) {
+      reviewsContent = (
+         <div id ={styles.reviewsContentPlaceholder}>There are currently no reviews for this activity... Leave one below!</div>
+      );
+   } else {
+      reviewsContent = (
          <div className={styles.reviewsContainer}>
             {reviews.map((review) => (
                <SingleReview key={review.id} review={review} />
             ))}
          </div>
+      );
+   }
+
+   return (
+      <div className={styles.reviewContainer}>
+         <h1>Reviews</h1>
+         {reviewsContent}
          <ReviewForm activityId={activityId} />
       </div>
    )
