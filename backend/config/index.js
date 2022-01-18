93a13cd1517704494085d3f514c44e5610f41386
tannerhladek <87441776+tannerhladek@users.noreply.github.com>
2022-01-18 14:32:35 -0800

    updated backend port to 5001 versus 5000

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,7 +1,7 @@
 // backend/config/index.js
 module.exports = {
    environment: process.env.NODE_ENV || 'development',
-   port: process.env.PORT || 5000,
+   port: process.env.PORT || 5001,
    db: {
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
